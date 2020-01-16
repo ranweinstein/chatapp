8425540f3f202f92197fcd36052f6ae4da4eac78
ranweinstein <ran.school@gmail.com>
2020-01-16 11:31:24 +0200

    Add files via upload

---
+++ base.asm
@@ -0,0 +1,24 @@
+IDEAL
+MODEL small
+STACK 20h
+DATASEG
+; --------------------------
+; Your variables here
+; --------------------------
+
+CODESEG
+start:
+    mov ax, @data
+    mov ds, ax
+    
+      
+; --------------------------
+; Your code here
+; --------------------------
+
+exit:
+    mov ax, 4c00h
+    int 21h
+END start
+
+
